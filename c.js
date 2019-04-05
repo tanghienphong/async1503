da131200303b3d1061629c50a30457c6d4594c76
Kelvin Zeng <tanghienphong@gmail.com>
2019-04-05 16:57:16 +0700

    commit

--- c.js
+++ c.js
@@ -12,8 +12,42 @@ function nhan(a,b, fn){
     })
 }
 
-nhan(2,3,(result, error)=>{
-    if(!error)
-        return console.log(result)
-    return console.log(error)
+function cong(a,b, fn){
+    if(isNaN(a) || isNaN(b) )
+        return fn(null,new Error('1 trong 2 tham số không phải là số!'))
+    const uri = `${URL}cong/${a}/${b}`;
+    request(uri,(error,res,body) => {
+        if(error) return fn(null, error)
+        let rs = JSON.parse(body)
+        return fn(rs.result)
+    })
+}
+
+function chia(a,b, fn){
+    if(isNaN(a) || isNaN(b) )
+        return fn(null,new Error('1 trong 2 tham số không phải là số!'))
+    const uri = `${URL}chia/${a}/${b}`;
+    request(uri,(error,res,body) => {
+        if(error) return fn(null, error)
+        let rs = JSON.parse(body)
+        return fn(rs.result)
+    })
+}
+
+//(3+5)*4/2
+cong(3,5,(tong, error)=>{
+    if(error)
+        return console.log(error)
+    nhan(tong,4,(tich,error) => {
+        if(error)
+        return console.log(error)
+        chia(tich,2,(result,error)=>{
+            if(error)
+                return console.log(error)
+                return console.log(result)
+        })
+    })
 })
+
+
+
